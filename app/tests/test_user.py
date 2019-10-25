45c16c980dfde9b861b390b62afbb8ffd14af8b2
sadegh <m.azarkaman@digikala.com>
2019-10-25 11:05:31 +0330

    login test

--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -14,7 +14,22 @@ def client():
         yield client
 
 
+def login(client):
+    ret = client.post('/login', data=dict(
+        username='admin',
+        password='123'
+    ))
+
+    return ret
+
+
 def test_user(client):
+    ret = login(client)
+
+    assert ret.status_code == 200
+
+    access_token = 'Bearer ' + json.loads(ret.get_data(as_text=True))['access_token']
+
     uname = randomString(10)
 
     ret = client.post('/user', data=dict(
@@ -24,7 +39,7 @@ def test_user(client):
         last_name="Azarkaman",
         phone_number="12345",
         birth_date=datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
-    ))
+    ), headers={'Authorization': access_token})
     user_id = json.loads(ret.get_data(as_text=True))["user_id"]
 
     assert int(user_id) > 0
@@ -34,11 +49,12 @@ def test_user(client):
         last_name="Azarkaman",
         phone_number="Pass123",
         birth_date=datetime.now().strftime('%Y-%m-%dT%H:%M:%S'),
-    ))
+    ), headers={'Authorization': access_token})
     assert ret.status_code == 204
     name = UserModel.find_by_id(user_id).firstName
     assert name == uname
-    ret = client.delete('/user/{}'.format(user_id))
+    ret = client.delete('/user/{}'.format(user_id),
+                        headers={'Authorization': access_token})
     assert ret.status_code is 202
 
 
